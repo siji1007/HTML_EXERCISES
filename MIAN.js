bb98de89ef28fa808235725e9ddf3ba6dc525511
Christian John Ibañez <109792510+siji1007@users.noreply.github.com>
2023-11-05 22:56:59 +0800

    Update MIAN.js

--- MIAN.js
+++ MIAN.js
@@ -5,20 +5,32 @@ const exerciseContent = {
   'AttributesExercise3': document.getElementById("AttributesExercise3").innerHTML,
   'AttributesExercise4': document.getElementById("AttributesExercise4").innerHTML,
   'AttributesExercise5': document.getElementById("AttributesExercise5").innerHTML,
+
+
+  'HeadingExercise1' : document.getElementById("HeadingExercise1").innerHTML,
+  'HeadingExercise2' : document.getElementById("HeadingExercise2").innerHTML,
+  'HeadingExercise3' : document.getElementById("HeadingExercise3").innerHTML,
+  'HeadingExercise4' : document.getElementById("HeadingExercise4").innerHTML,
+  'HeadingExercise5' : document.getElementById("HeadingExercise5").innerHTML,
+  
 };
 
-function Test(exercise) {
-  const ExerciseArea = document.querySelector(".exercises-content");
-  console.log(exercise);
-
-  if (exerciseContent.hasOwnProperty(exercise)) {
-      ExerciseArea.innerHTML = exerciseContent[exercise];
-  } else {
-      ExerciseArea.innerHTML = 'Exercise not found';
-  }
-}
- 
 document.addEventListener("DOMContentLoaded", function () {
+
+
+  var computerCode = document.getElementById("CC-exercises").innerHTML;
+  console.log(computerCode);
+
+
+
+
+
+  var ExerciseArea = document.querySelector(".exercises-content");  
+  ExerciseArea.innerHTML = "";
+
+  var HeadingExercise_contents = document.querySelector(".HeadingExercise_contents");
+  HeadingExercise_contents.innerHTML = "";
+
   // add hovered class to the selected list item
   let list = document.querySelectorAll(".navigation li");
 
@@ -43,24 +55,37 @@ document.addEventListener("DOMContentLoaded", function () {
     navigation.classList.toggle("active");
     main.classList.toggle("active");
   };
-
-
   const sidebarItems = document.querySelectorAll(".sidebar-container li");
-
-
   const contentArea = document.querySelector(".content");
 
-  const ExerciseArea = document.querySelector(".exercises-content");
-  ExerciseArea.innerHTML = " ";
 
-  // Define the content for each attribute (you can replace this with your actual content)
   const attributeContent = {
     Attributes: document.getElementById("Attributes-exercises").innerHTML,
     Headings: document.getElementById("Headings-exercises").innerHTML,
-    // Add more content for other sections here
+    Paragraph: document.getElementById("Paragraph-exercises").innerHTML,
+    Styles: document.getElementById("Styles-exercises").innerHTML,
+    Formatting: document.getElementById("Formatting-exercises").innerHTML,
+    Quotations: document.getElementById("Quotations-exercises").innerHTML,
+    Comments: document.getElementById("Comments-exercises").innerHTML,
+    CSS: document.getElementById("Css-exercises").innerHTML,
+    Links: document.getElementById("Links-exercises").innerHTML,
+    Images: document.getElementById("Images-exercises").innerHTML,
+    Tables: document.getElementById("Tables-exercises").innerHTML,
+    List: document.getElementById("List-exercises").innerHTML,
+    Classes: document.getElementById("Classes-exercises").innerHTML,
+    ID: document.getElementById("ID-exercises").innerHTML,
+    IFframe: document.getElementById("IFframe-exercises").innerHTML,
+    Scripts: document.getElementById("Scripts-exercises").innerHTML,
+    Computer_Code: document.getElementById("CC-exercises").innerHTML,
+    Forms: document.getElementById("Forms-exercises").innerHTML,
+    Forms_Attributes: document.getElementById("FormsAttri-exercises").innerHTML,
+    Forms_Elements: document.getElementById("FormsElemets-exercises").innerHTML,
+    Input_Types: document.getElementById("InputType-exercises").innerHTML,
+    Input_Attributes: document.getElementById("InputAtt-exercises").innerHTML,
+  
     
   };
- 
+
   contentArea.innerHTML = " ";
   sidebarItems.forEach((item) => {
     item.addEventListener("click", (event) => {
@@ -68,11 +93,37 @@ document.addEventListener("DOMContentLoaded", function () {
       const itemText = item.textContent.trim();
       navigation.classList.remove("active");
       main.classList.remove("active");
-
       contentArea.innerHTML = attributeContent[itemText];
+      
     });
-
   }); 
+
 });
+
+
+function Test(exercise) {
+  var ExerciseArea = document.querySelector(".exercises-content");
+  var HeadingExercise_contents = document.querySelector(".HeadingExercise_contents");
+
+  if (exerciseContent.hasOwnProperty(exercise)) {
+    if (ExerciseArea) {
+      ExerciseArea.innerHTML = exerciseContent[exercise];
+    } else if (HeadingExercise_contents) {
+      HeadingExercise_contents.innerHTML = exerciseContent[exercise];
+    }
+
+
+  } else {
+    if (ExerciseArea) {
+      ExerciseArea.innerHTML = '';
+    }
+    if (HeadingExercise_contents) {
+      HeadingExercise_contents.innerHTML = '';
+    }
+  }
+}
+
+
+
 
 
